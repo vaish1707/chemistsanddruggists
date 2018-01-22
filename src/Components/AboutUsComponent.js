f62b7e65de16d9535ea3bd70cc8051e0324431db
selvaganeshr <selvaganeshrajam@gmail.com>
2018-01-22 19:21:39 +0530

    pdf download added styles issue fixes

--- src/Components/AboutUsComponent.js
+++ src/Components/AboutUsComponent.js
@@ -1,10 +1,16 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import '../index.css';
 
 class AboutUsComponent extends Component {
 
     constructor(props) {
         super(props);
+        this.readMore = this.readMore.bind(this);
+    }
+
+    readMore() {
+        console.log("Readmore");
+
     }
 
     render() {
@@ -13,21 +19,14 @@ class AboutUsComponent extends Component {
                 <div className="container text-center my-auto">
                     <h1 className="mb-1">About Us</h1>
                     <p className="aboutus-text">
-                        Who We Are?
-SenSemColours is the new brainchild and an ambitious startup of Mr. Tamijarassane and Mrs. Gnanasoundary who wants to redefine “PRINTING INK” manufacturing industry by applying a unique blend of innovative services and outstanding customer experiences.
-
-Having started his career in 1998 as a marketing manager with a similar startup, Mr. Tamijarassane, over the period of 19 years has gained an extensive experience in the Printing Ink industry that covered the entire Southern region of India. He also has contributed comprehensively in varied departments such as accounts and technical details in his previous stint in addition to being a marketing manager.
-
-Mr. Tamijarassane himself holds a Bachelor’s degree (1995) in Physics from Pondicherry University whereas his sole partner Mrs. Gnanasoundary holds a Bachelor’s in Zoology (2001) from Krishnaswamy College in Cuddalore, Tamil Nadu.Their new yet vastly experienced (more than 18 years in printing ink manufacturing) team at SenSemColours are formed from the core of wildest dreams combined with positiveness, enthusiasm, business and financial acumen. Thus, promises doing business with them would be a stellar encounter and are very much looking forward to have you onboard.
-
-Our motto - Customer, Company, Team.
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters, as opposed to using 'Content here, content here', making it look like readable English. Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, and a search for 'lorem ipsum' will uncover many web sites still in their infancy. Various versions have evolved over the years, sometimes by accident, sometimes on purpose (injected humour and the like).
 </p>
-                    
+                    <a className="btn btn-primary btn-xl js-scroll-trigger" onClick={this.readMore.bind(this)} href="/test.pdf">Read More</a>
                 </div>
                 <div className="overlay"></div>
             </header>
         </div>)
-        
+
     }
 }
 
--- src/Components/NavBarComponent.js
+++ src/Components/NavBarComponent.js
@@ -7,7 +7,7 @@ class NavBarComponent extends Component {
         console.log(this.props);
     }
     render() {
-        return (<div>
+        return (<div className="banner">
             <a className="menu-toggle rounded" href="#">
                 <i className="fa fa-bars"></i>
             </a><nav id="sidebar-wrapper">
@@ -32,16 +32,23 @@ class NavBarComponent extends Component {
                     </li>
                 </ul>
             </nav>
-            <header className="masthead d-flex">
+            <div>
+            <div className="masthead d-flex">
                 <div className="container text-center my-auto">
-                    <h1 className="mb-1">{this.props.title}</h1>
-                    <h3 className="mb-5">
-                        <em>{this.props.tag}</em>
-                    </h3>
+                <div className="banner-logo">
+                        <img src="http://www.wlac.org/assets/siteDesq/5223/gallery/capital-chemist-logo.png">
+                        </img>
+                    </div>
+                    <h3 className="mb-1">{this.props.title}</h3>
+                   
+                    <h5 className="mb-5">
+                        {this.props.tag}
+                    </h5>
                     <a className="btn btn-primary btn-xl js-scroll-trigger" href="#about">{this.props.stepIn}</a>
                 </div>
                 <div className="overlay"></div>
-            </header></div>)
+            </div>
+            </div></div>)
     }
 
 }
