7c9be3949280ca1b84c7e5eefa4488589361db7f
selvaganeshrajam@gmail.com <selvaganeshrajam@gmail.com>
2018-01-23 15:11:16 +0530

    moved names to seperate file

---
+++ src/data/names.js
@@ -0,0 +1,56 @@
+const OfficeBearerNames = [{
+    bearer: "W.S. Mohan Kumar",
+    desc: "President"
+}, { bearer: "T.Natarajan", desc: "General Secretary" }, {
+    bearer: "P.Thirulogachandar",
+    desc: "Treasurer"
+}, { bearer: "J.Vijayan", desc: "Vice President" }, {
+    bearer: "Balasubramaniam (Tvt)",
+    desc: "Vice President"
+}, { bearer: "Bhaktachalam", desc: "Vice President" },
+{ bearer: "Sudhakar (GTPT)", desc: "Vice President" }, {
+    bearer: "Periyakaruppan",
+    desc: "Vice President"
+}, { bearer: "Thiruselvam", desc: "Vice President" }, {
+    bearer: "Sai Nathan",
+    desc: "Joint Secretary"
+}, { bearer: "Palaniappan", desc: "Joint Secretary" }, {
+    bearer: "Stalin",
+    desc: "Joint Secretary"
+}, { bearer: "Venkatavardhan", desc: "Joint Secretary" }, {
+    bearer: "Gnanavel Murugan",
+    desc: "Joint Secretary"
+}, { bearer: "Jagan", desc: "Joint Secretary" },
+{
+    bearer: "Jeevanathan",
+    desc: "Organisation Secretary"
+}, { bearer: "G. Thiyagarajan", desc: "Administration Secretary" }, {
+    bearer: "V. Sakthivelan",
+    desc: "Chairman,Co-ordination Committee"
+}, { bearer: "Srinivasan", desc: "Chairman,Constitution Committee" }, {
+    bearer: "G.Krishnan",
+    desc: "Chairman, Retailwing"
+}, { bearer: "Marudai Periyasamy", desc: "Chairman, Wholesalewing" }, {
+    bearer: "S.A.Ramesh",
+    desc: "Chairman, Trade & Industry Relation"
+}, { bearer: "A.O.Selvam", desc: "Secretary, Company affairs" }, {
+    bearer: "K.A.Prem Kumar",
+    desc: "Chairman, Trade Reform Committee"
+}];
+
+
+const Advisors = [{
+    bearer: "M. ArulKumar",
+    desc: ""
+}, { bearer: "C.Dinakaran", desc: "" }, {
+    bearer: "Elumalai",
+    desc: ""
+}, { bearer: "Shankar Gupta", desc: "" }, {
+    bearer: "Srinivasan",
+    desc: ""
+}];
+
+module.exports = {
+    "OfficeBearerNames": OfficeBearerNames,
+    "Advisors": Advisors
+}
